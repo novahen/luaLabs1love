bb84fced769dfad8350d3d5fb4887770f05a3f54
FC <maks.tsvet@mail.ru>
2020-09-15 12:38:39 +0300

    SC

--- lab-02.lua
+++ lab-02.lua
@@ -11,7 +11,63 @@ bld:add_from_file('lab-02.glade')
 ui = bld.objects
 
 function ui.btn_sum:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+ui.res.label = a+b
+end
 
+function ui.btn_sub:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+ui.res.label = a-b
+end
+
+function ui.btn_multi:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+ui.res.label = a*b
+end
+
+function ui.btn_div:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+if (b == 0) then
+ui.res.label = 'Error'
+else
+ui.res.label = a/b
+
+end
+end
+
+function ui.btn_pow:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+
+ui.res.label = a^b
+
+end
+
+function ui.btn_sqrt:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+if (a<0) then
+ui.res.label = 'Error'
+else
+ui.res.label = math.sqrt(a)
+
+end
+end
+
+
+function ui.btn_log:on_clicked(...)
+a = tonumber(ui.text1.text)
+b = tonumber(ui.text2.text)
+if (a<0) then
+ui.res.label = 'Error'
+else
+ui.res.label = math.log10(a)
+
+end
 end
 
 ui.calc.title = 'lab-02_Tsvetkov'
